802d2d25e8044aa88fe5cfb4fd1e8a4bfdc364d3
AlCorreia <alvaro.hc.correia@gmail.com>
2023-03-06 17:09:12 +0100

    Passing log_w to decoders

--- models/cm.py
+++ models/cm.py
@@ -1,7 +1,7 @@
 from torch.utils.data import DataLoader
 from torch.distributions import Normal
 from typing import Callable, Optional
-from utils.losses import bce, mse, ce
+from utils.losses import bce_loss, mse_loss, ce_loss
 import pytorch_lightning as pl
 from tqdm import tqdm
 import torch.nn as nn
@@ -37,12 +37,17 @@ class CLTBernoulliDecoder(nn.Module):
     def forward(
         self,
         x: torch.Tensor,
+        log_w: torch.Tensor,
         z: torch.Tensor,
+        k: Optional[int] = None,
         missing: Optional[bool] = None,
         n_chunks: Optional[int] = None
     ):
         assert missing is False, 'Marginalisation is not yet implemented!'
         assert x.ndim == 2, 'Only tabular data are allowed!'
+
+        if k is not None:
+            raise NotImplementedError
 
         logits_p = self._preprocess_logits_p(self.net(z))
         x_cond = torch.stack([1 - x[:, self.tree], x[:, self.tree]], dim=2)
@@ -51,7 +56,7 @@ class CLTBernoulliDecoder(nn.Module):
         log_prob_chunks = []
         logits_p_chunks = tuple([logits_p]) if n_chunks is None else logits_p.chunk(n_chunks, dim=0)
         for logits_p_chunk in logits_p_chunks:
-            lls = bce(logits_p_chunk, x_doubled, dim_start_sum=None)
+            lls = bce_loss(logits_p_chunk, x_doubled, dim_start_sum=None)
             log_prob_chunks.append(torch.einsum('bijk, bjk -> bij', lls, x_cond).sum(-1))
         log_prob_bins = torch.cat(log_prob_chunks, dim=1)
         return log_prob_bins
@@ -69,36 +74,50 @@ class BernoulliDecoder(nn.Module):
     def forward(
         self,
         x: torch.Tensor,
+        log_w: torch.Tensor,
         z: torch.Tensor,
+        k: Optional[int] = None,
         missing: Optional[bool] = None,
         n_chunks: Optional[int] = None
     ):
+        if k is not None:
+            raise NotImplementedError
+
         logits_p = self.net(z)
         logits_p_chunks = tuple([logits_p]) if n_chunks is None else logits_p.chunk(n_chunks, dim=0)
-        log_prob_bins = torch.cat([bce(logits_p_chunk, x, missing) for logits_p_chunk in logits_p_chunks], dim=1)
-        return log_prob_bins
-    
-    
-class CategoricalDecoder(nn.Module):
+        log_prob_bins = torch.cat([bce_loss(logits_p_chunk, x, missing) for logits_p_chunk in logits_p_chunks], dim=1)
+        return log_prob_bins + log_w.unsqueeze(0)
 
+
+class CategoricalDecoder(nn.Module):
     def __init__(
-        self,
-        net
-    ):
+            self,
+            net: nn.Module):
         super(CategoricalDecoder, self).__init__()
         self.net = net
 
     def forward(
-        self,
-        x: torch.Tensor,
-        z: torch.Tensor,
-        missing: Optional[bool] = None,
-        n_chunks: Optional[int] = None
+            self,
+            x: torch.Tensor,
+            log_w: torch.Tensor,
+            z: torch.Tensor,
+            k: Optional[int] = None,
+            missing: Optional[bool] = None,
+            n_chunks: Optional[int] = None,
     ):
-        logits_p = self.net(z)
-        logits_p_chunks = tuple([logits_p]) if n_chunks is None else logits_p.chunk(n_chunks, dim=0)
-        log_prob_bins = torch.cat([ce(logits_p_chunk, x, missing) for logits_p_chunk in logits_p_chunks], dim=1)
-        return log_prob_bins
+        z_chunks = tuple([z]) if n_chunks is None else z.chunk(n_chunks, dim=0)
+        if k is not None:
+            with torch.no_grad():
+                # Run approximate posterior to find the 'best' k z values for each x
+                log_prob_bins = torch.cat([ce_loss(self.net(z_chunk), x, k=None, missing=missing) for z_chunk in z_chunks],
+                                          dim=1)
+                log_prob_bins = log_prob_bins + log_w.unsqueeze(0)
+                z_top_k = z[torch.topk(log_prob_bins, k=k, dim=-1)[1]]  # shape (batch_size, k, latent_dim)
+            log_prob_bins_top_k = ce_loss(self.net(z_top_k.view(batch_size * k, -1)), x, k=k, missing=missing)
+            return log_prob_bins_top_k
+        else:
+            log_prob_bins = torch.cat([ce_loss(self.net(z_chunk), x, missing) for z_chunk in z_chunks], dim=1)
+            return log_prob_bins + log_w.unsqueeze(0)
 
 
 class GaussianDecoder(nn.Module):
@@ -121,10 +140,15 @@ class GaussianDecoder(nn.Module):
     def forward(
         self,
         x: torch.Tensor,
+        log_w: torch.Tensor,
         z: torch.Tensor,
+        k: Optional[int] = None,
         missing: Optional[bool] = None,
         n_chunks: Optional[int] = None
     ):
+        if k is not None:
+            raise NotImplementedError
+
         if self.learn_std:
             mu_logvar = self.net(z)
             mu, logvar = mu_logvar.chunk(2, dim=1)
@@ -136,8 +160,8 @@ class GaussianDecoder(nn.Module):
             std = torch.full_like(mu, fill_value=self.min_std)
         mu_std_chunks = [[mu], [std]] if n_chunks is None else [[*mu.chunk(n_chunks, 0)], [*std.chunk(n_chunks, 0)]]
         log_prob = torch.cat(
-            [mse(mu_std_chunks[0][i], mu_std_chunks[1][i], x, missing) for i in range(len(mu_std_chunks[0]))], dim=1)
-        return log_prob
+            [mse_loss(mu_std_chunks[0][i], mu_std_chunks[1][i], x, missing) for i in range(len(mu_std_chunks[0]))], dim=1)
+        return log_prob + log_w.unsqueeze(0)
 
     def sample_continuous(
         self,
@@ -178,10 +202,12 @@ class ContinuousMixture(pl.LightningModule):
         self,
         decoder: nn.Module,
         sampler: Callable,
+        k: Optional[int] = None
     ):
         super(ContinuousMixture, self).__init__()
         self.decoder = decoder
         self.sampler = sampler
+        self.k = k
         self.n_chunks = None
         self.missing = None
         self.save_hyperparameters(ignore=['n_chunks', 'missing'])
@@ -191,15 +217,15 @@ class ContinuousMixture(pl.LightningModule):
         x: torch.Tensor,
         z: Optional[torch.Tensor] = None,
         log_w: Optional[torch.Tensor] = None,
+        k: Optional[int] = None,
         seed: Optional[int] = None
     ):
         assert (z is None and log_w is None) or (z is not None and log_w is not None)
         if z is None:
             z, log_w = self.sampler(seed=seed)
-        log_prob_bins = self.decoder.forward(x, z.to(x.device), self.missing, self.n_chunks)
+        log_prob_bins = self.decoder.forward(x, log_w.to(x.device), z.to(x.device), k, self.missing, self.n_chunks)
         assert log_prob_bins.size() == (x.size(0), z.size(0))
-
-        log_prob = torch.logsumexp(log_prob_bins + log_w.to(x.device).unsqueeze(0), dim=1, keepdim=False)
+        log_prob = torch.logsumexp(log_prob_bins, dim=1, keepdim=False)
         return log_prob
 
     def training_step(
@@ -207,7 +233,7 @@ class ContinuousMixture(pl.LightningModule):
         x: torch.Tensor,
         batch_idx: int
     ):
-        log_prob = self.forward(x)
+        log_prob = self.forward(x, k=self.k)
         loss = (-log_prob).mean()
         self.log('train_loss', loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
         return loss
@@ -217,7 +243,7 @@ class ContinuousMixture(pl.LightningModule):
         x: torch.Tensor,
         batch_idx: int
     ):
-        log_prob = self.forward(x, seed=42)
+        log_prob = self.forward(x, k=None, seed=42)
         loss = (-log_prob).mean()
         self.log('valid_loss', loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
         return loss
@@ -234,7 +260,7 @@ class ContinuousMixture(pl.LightningModule):
     ):
         self.eval()
         loader = tqdm(loader) if progress_bar else loader
-        lls = torch.cat([self.forward(x.to(device), z, log_w, seed) for x in loader], dim=0)
+        lls = torch.cat([self.forward(x.to(device), z, log_w, k=None, seed) for x in loader], dim=0)
         assert len(lls) == len(loader.dataset)
         return lls
 

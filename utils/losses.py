f7a8ed01269b4eefbf1fe6646c1e3a558f86e516
AlCorreia <alvaro.hc.correia@gmail.com>
2023-03-06 17:08:33 +0100

    Updating and renaming loss functions

--- utils/losses.py
+++ utils/losses.py
@@ -2,10 +2,10 @@ from typing import Optional
 from numbers import Real
 import torch
 import math
-from torch.distributions import Independent, Categorical
+from torch.distributions import Categorical
 
 
-def bce(
+def bce_loss(
     logits_p: torch.Tensor,
     x: torch.Tensor,
     missing: Optional[bool] = None,
@@ -13,20 +13,22 @@ def bce(
     eps: float = 1e-7
 ):
     x = x.unsqueeze(1)
+    if missing is None or missing is True:
+        mask = ~ torch.isnan(x)
+    else:
+        mask = torch.ones_like(x)
+
     x = torch.nan_to_num(x)
     p = torch.clip(logits_p.sigmoid(), eps, 1 - eps)
     log_prob = x * torch.log(p + eps) + (1 - x) * torch.log(1 - p + eps)
-
-    if missing is None or missing is True:
-        mask = ~ torch.isnan(x)
-        log_prob = log_prob * mask
+    log_prob = log_prob * mask
 
     if dim_start_sum is not None:
         log_prob = log_prob.sum(dim=[i for i in range(dim_start_sum, log_prob.ndim)])
     return log_prob
 
 
-def mse(
+def mse_loss(
     loc: torch.Tensor,
     scale: torch.Tensor,
     x: torch.Tensor,
@@ -46,14 +48,46 @@ def mse(
     return log_prob
 
 
-def ce(
+def ce_loss(
     logits_p: torch.Tensor,
     x: torch.Tensor,
+    k: Optional[int] = None,
     missing: Optional[bool] = None,
     dim_start_sum: Optional[int] = 2,
-    eps: float = 1e-7
 ):
+
+    batch_size, data_dim = x.shape[0], x.shape[1:]
+    n_bins = logits_p.shape[0]
+    logits_p = logits_p.view(n_bins, -1, *data_dim)
+
+    if missing is None or missing is True:
+        mask = ~ torch.isnan(x)
+    else:
+        mask = torch.ones_like(x)
     x = torch.nan_to_num(x)
-    dist = Independent(Categorical(logits=logits_p.permute(0, 2, 3, 1)), x.ndim - 2)
-    log_prob = dist.log_prob(x.float())
+
+    if k is None:
+        # Categorical probs as last dimension
+        logits_p = torch.movedim(logits_p, 1, -1)
+        # Create categorical distribution
+        dist = Categorical(logits=logits_p)
+        # Compute log-probs for all combinations of x and z (logits) values
+        # This is done by unsqueezing the first dimension in x
+        log_prob = dist.log_prob(x.unsqueeze(1).float())
+        # Expand mask to broadcast to every pair (x, z)
+        mask = mask.unsqueeze(1)
+    else:
+        assert batch_size == int(n_bins / k)
+        # Categorical probs as last dimension
+        logits_p = torch.movedim(logits_p, 1, -1)
+        # Create categorical distribution
+        dist = Categorical(logits=logits_p)
+        # Compute log-probs for each of x against corresponding k logits
+        # This is done by expanding the first dimension of x to batch_size * k
+        x = x.repeat_interleave(k, dim=0)
+        log_prob = dist.log_prob(x.float())
+
+    log_prob = log_prob * mask
+    if dim_start_sum is not None:
+        log_prob = log_prob.sum(dim=[i for i in range(dim_start_sum, log_prob.ndim)])
     return log_prob
